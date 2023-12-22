5adb528c4c0b1dcb3bc98e5568a04930b3d3055f
OGMONARCH <149230226+OGMONARCH@users.noreply.github.com>
2023-12-22 23:07:40 +0100

    heyooo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
+ const [properties, setProperties] = useState([]);
+ const [filteredProperties, setFilteredProperties] = useState([]);
+
+ useEffect(() => {
+    fetch('https://api.datausa.io/api/?key=YOUR_API_KEY')
+      .then(response => response.json())
+      .then(data => {
+        setProperties(data);
+        setFilteredProperties(data);
+      });
+ }, []);
+
+ const filterProperties = (price, location) => {
+    const filtered = properties.filter(property => {
+      let isWithinPriceRange = true;
+      let isWithinLocation = true;
+
+      if (price.min) {
+        isWithinPriceRange = property.price >= price.min;
+      }
+
+      if (price.max) {
+        isWithinPriceRange = property.price <= price.max;
+      }
+
+      if (location) {
+        isWithinLocation = property.location === location;
+      }
+
+      return isWithinPriceRange && isWithinLocation;
+    });
+
+    setFilteredProperties(filtered);
+ };
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter onFilter={filterProperties} />
+      <PropertyList properties={filteredProperties} />
     </div>
-  );
+ );
+}
+
+function Filter({ onFilter }) {
+ const [form, setForm] = useState({ price: { min: '', max: '' }, location: '' });
+
+ const handleFilter = () => {
+    onFilter(form.price, form.location);
+ };
+
+ const handleChange = event => {
+    const { name, value } = event.target;
+    setForm(prevState => ({ ...prevState, [name]: value }));
+ };
+
+ return (
+    <div>
+      <label>Min Price</label>
+      <input name="price.min" value={form.price.min} onChange={handleChange} />
+      <label>Max Price</label>
+      <input name="price.max" value={form.price.max} onChange={handleChange} />
+      <label>Location</label>
+      <input name="location" value={form.location} onChange={handleChange} />
+      <button onClick={handleFilter}>Filter</button>
+    </div>
+ );
+}
+
+function PropertyList({ properties }) {
+ return (
+    <div className="property-list">
+      {properties.map(property => (
+        <div className="property-card" key={property.id}>
+          <h2>{property.title}</h2>
+          <p>Price: {property.price}</p>
+          <p>Location: {property.location}</p>
+        </div>
+      ))}
+    </div>
+ );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default data =()=>{
+    [{
+        "id": 1,
+        "property_name": "apartment",
+        "location ": "California",
+        "price": "$3920.00",
+        "categories ": "Rebar & Wire Mesh Install",
+        "categories": "commercial"
+      }, {
+        "id": 2,
+        "property_name": "house",
+        "location ": "Arkansas",
+        "price": "$65606.04",
+        "categories ": "Granite Surfaces",
+        "categories": "industrial"
+      }, {
+        "id": 3,
+        "property_name": "apartment",
+        "location ": "California",
+        "price": "$63574.59",
+        "categories ": "Sitework & Site Utilities",
+        "categories": "residential"
+      }, {
+        "id": 4,
+        "property_name": "apartment",
+        "location ": "California",
+        "price": "$50202.46",
+        "categories ": "Prefabricated Aluminum Metal Canopies",
+        "categories": "industrial"
+      }, {
+        "id": 5,
+        "property_name": "house",
+        "location ": "California",
+        "price": "$81831.50",
+        "categories ": "Roofing (Asphalt)",
+        "categories": "industrial"
+      }, {
+        "id": 6,
+        "property_name": "townhouse",
+        "location ": "Arkansas",
+        "price": "$79619.67",
+        "categories ": "Epoxy Flooring",
+        "categories": "residential"
+      }]
+      
+}
